94ad5874dda31395347121498d79e69ecc3d9b4e
Morning <morning@mstars-lab3-13s-iMac.local>
2023-02-06 12:45:18 +0800

    adsd

---
+++ controllers/products.controller.js
@@ -0,0 +1,98 @@
+const fs = require("fs");
+const { parse } = require("path");
+const uuid = require("uuid");
+
+const dataFile = process.cwd() + "/data/products.json"
+
+exports.getAll = (req, res) => {
+    fs.readFile(dataFile, "utf-8", (readErr, data) => {
+
+        if (readErr) {
+            return res.json({ status: false, result: readErr })
+        }
+        const savedData = JSON.parse(data);
+        return res.json({ status: true, result: savedData })
+    })
+}
+exports.get = (req, res) => {
+    const { id } = req.params
+    fs.readFile(dataFile, "utf-8", (readErr, data) => {
+        if (readErr) {
+            return res.json({ status: false, message: readErr })
+        }
+        const myData = JSON.parse(data)
+        const savedData = myData.filter((e) => e.id == id)
+
+        return res.json(savedData)
+    })
+}
+
+
+exports.create = (req, res) => {
+    const { productName, price, thumbnail, image, salePercentage, quantity, description, createdUser, category } = req.body;
+
+    fs.readFile(dataFile, "utf-8", (readErr, data) => {
+        if (readErr) {
+            return res.json({ status: false, message: readErr })
+        }
+
+        const parsedDAta = data ? JSON.parse(data) : [];
+        const newObj = { id: uuid.v4(), categoryId: uuid.v4(), productName, price, thumbnail, image, salePercentage, quantity, description, createdUser, category };
+
+        parsedDAta.push(newObj);
+
+        fs.writeFile(dataFile, JSON.stringify(parsedDAta), (writeErr) => {
+            if (writeErr) {
+                return res.json({ status: false, message: writeErr })
+            }
+            return res.json({ status: true, result: parsedDAta })
+        })
+    })
+}
+
+exports.update = (req, res) => {
+    const { id } = req.params
+    const { productName, price, thumbnail, image, salePercentage, quantity, description, createdUser, category } = req.body;
+
+    fs.readFile(dataFile, "utf-8", (readErr, data) => {
+        if (readErr) {
+            return res.json({ status: false, message: readErr })
+        }
+        const parsedData = JSON.parse(data)
+        const updateData = parsedData.map((cateObj) => {
+            if (cateObj.id == id) {
+                return {
+                    ...cateObj,
+                    productName, price, thumbnail, image, salePercentage, quantity, description, createdUser, category
+                }
+            }
+            else {
+                return cateObj;
+            }
+        });
+        fs.writeFile(dataFile, JSON.stringify(updateData), (writeErr) => {
+            if (writeErr) {
+                return res.json({ status: false, message: writeErr })
+            }
+            return res.json({ status: true, result: updateData })
+        })
+    })
+}
+
+exports.delete = (req, res) => {
+    const { id } = req.params;
+    fs.readFile(dataFile, "utf-8", (readErr, data) => {
+        if (readErr) {
+            return res.json({ status: false, message: readErr })
+        }
+        const parsedDAta = JSON.parse(data);
+        const deletedData = parsedDAta.filter((e) => e.id != id);
+
+        fs.writeFile(dataFile, JSON.stringify(deletedData), (writeErr) => {
+            if (writeErr) {
+                return res.json({ status: false, message: writeErr })
+            }
+            return res.json({ status: true, result: deletedData })
+        })
+    })
+}
