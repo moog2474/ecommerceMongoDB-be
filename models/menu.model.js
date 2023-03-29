87283bf5d57ed73c60207afca4c983954a36fd9c
Morning <morning@mstars-lab3-13s-iMac.local>
2023-03-29 12:33:33 +0800

    xax

--- models/menu.model.js
+++ models/menu.model.js
@@ -1,8 +1,9 @@
-const {default: mongoose} = require("mongoose");
+const {default: mongoose, Schema} = require("mongoose");
 
 const menuName = new mongoose.Schema (
     {
         menuName: {type: String, unique: true},
+        type: {type: Schema.Types.Mixed, enum: ["front", "back"]},
         link: String
     },
     {collection: 'Menu'}
